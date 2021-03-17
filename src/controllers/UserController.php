0a5ebbf20b91d7397c323032024eac8abbb1ef95
Art0r <arthur0139@gmail.com>
2021-03-17 18:43:37 -0300

    Rota e controller para update implementados

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -1,5 +1,6 @@
 <?php
     require_once "src/model/Database.php";
+    define("table", "users");
 
     class UserController {
         private $conn;
@@ -17,15 +18,23 @@
         }
 
         public function getAllUsers(){
-            $results = $this->query("SELECT * FROM users");
+            $results = $this->query("SELECT * FROM ". table);
 
             return $results;
         }
 
         public function getUser($id){
-            $results = $this->query("SELECT * FROM users WHERE id = ?", [$id]);
+            $results = $this->query("SELECT * FROM " . table . " WHERE id = ?", [$id]);
 
             return $results;
+        }
+
+        public function createUser($name, $email) {
+            $this->query("INSERT INTO " . table . "(name, email) VALUES (?, ?)", [$name, $email]);  
+        }
+
+        public function updateUser($id, $newName, $newEmail) {
+            $this->query("UPDATE " . table . " SET name = ?, email = ? WHERE id = ?", [$newName, $newEmail, $id]);
         }
     }
 ?>
--- src/index.php
+++ src/index.php
@@ -6,6 +6,9 @@
     use Slim\Factory\AppFactory;
 
     $app = AppFactory::create();
+    $app->addBodyParsingMiddleware();
+    $app->addRoutingMiddleware();
+    $app->addErrorMiddleware(true, true, true);
 
     $app->get("/user", function(Request $req, Response $res) {
         $userController = new UserController();
@@ -24,6 +27,44 @@
         
         $res->getBody()->write($json);
         return $res->withHeader("Content-type", "application/json");
+    });
+
+    $app->post("/user", function(Request $req, Response $res) {
+        $userController = new UserController();
+        $params = $req->getParsedBody();
+
+        $userController->createUser($params["name"], $params["email"]);
+
+        $json = json_encode(array(
+            "sucess" => true,
+        ));
+
+        $res->getBody()->write($json);
+        return $res->withHeader("Content-type", "application/json");
+    });
+
+    $app->put("/user/{id}", function(Request $req, Response $res, $args){
+        $userController = new UserController();
+        $params = $req->getParsedBody();
+        $id = $args["id"];
+
+        if (!isset($params["name"])){
+            $user = $userController->getUser($id);
+            $userController->updateUser($id, $user[0]["name"], $params["email"]);
+        } else if (!isset($params["email"])) {
+            $user = $userController->getUser($id);
+            $userController->updateUser($id, $params["name"], $user[0]["email"]);
+        } else {
+            $userController->updateUser($id, $params["name"], $params["email"]);
+        }
+
+        $json = json_encode(array(
+            "sucess" => true,
+        ));
+
+        $res->getBody()->write($json);
+        return $res->withHeader("Content-type", "application/json");
+
     });
 
     $app->run();
